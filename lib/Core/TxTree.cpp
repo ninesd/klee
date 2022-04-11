fa030a119e0b3248be48e40327458198443e4dd0
kuon <503129061@qq.com>
2022-04-11 16:18:56 +0800

    Fix bugs

--- lib/Core/TxTree.cpp
+++ lib/Core/TxTree.cpp
@@ -1617,7 +1617,7 @@ setDebugSubsumptionLevelTxTree(debugSubsumptionLevel);
             z3solver->setCoreSolverTimeout(timeout);
             success = z3solver->directComputeValidity(
                 Query(state.constraints, expr), result, unsatCore);
-            z3solver->setCoreSolverTimeout(time::Span("0ms"));
+            z3solver->setCoreSolverTimeout(time::Span());
             delete z3solver;
           } else {
             solver->setTimeout(timeout);
@@ -2307,10 +2307,6 @@ TxTree::TxTree(
 bool TxTree::subsumptionCheck(TimingSolver *solver, ExecutionState &state,
                               time::Span timeout) {
   assert(state.txTreeNode == currentTxTreeNode);
-
-  klee_message("Subsumption check for Node #%lu, Program Point %lu",
-               state.txTreeNode->getNodeSequenceNumber(),
-               state.txTreeNode->getProgramPoint());
 
   // Immediately return if the state's instruction is not the
   // the interpolation node id. The interpolation node id is the
