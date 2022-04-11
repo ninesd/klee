8924d45154b1384c74dd0d7e9dde4248d3d3c73b
kuon <503129061@qq.com>
2022-04-11 21:30:09 +0800

    Fix bugs

--- include/klee/Expr/ExprVisitor.h
+++ include/klee/Expr/ExprVisitor.h
@@ -85,6 +85,7 @@ namespace klee {
     virtual Action visitSle(const SleExpr&);
     virtual Action visitSgt(const SgtExpr&);
     virtual Action visitSge(const SgeExpr&);
+    virtual Action visitExists(const ExistsExpr&);
     virtual Action visitFOEq(const FOEqExpr &);
     virtual Action visitFOLt(const FOLtExpr &);
     virtual Action visitFOLe(const FOLeExpr &);
