b500c70fd5f2674ef15658f47d6d261116284c0f
kuon <503129061@qq.com>
2022-04-13 14:23:45 +0800

    Fix bugs

--- lib/Solver/Z3CoreBuilder.h
+++ lib/Solver/Z3CoreBuilder.h
@@ -49,7 +49,6 @@ protected:
   Z3ASTHandle bvLeExpr(Z3ASTHandle lhs, Z3ASTHandle rhs) override;
   Z3ASTHandle sbvLtExpr(Z3ASTHandle lhs, Z3ASTHandle rhs) override;
   Z3ASTHandle sbvLeExpr(Z3ASTHandle lhs, Z3ASTHandle rhs) override;
-  Z3ASTHandle sbvLeExpr(Z3ASTHandle lhs, Z3ASTHandle rhs) override;
 
   Z3ASTHandle constructAShrByConstant(Z3ASTHandle expr, unsigned shift,
                                       Z3ASTHandle isSigned) override;
